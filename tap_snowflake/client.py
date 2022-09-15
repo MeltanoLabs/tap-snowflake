199d218d4baf183eb9c79e68f21b919033a36fa8
Ken Payne <ken@meltano.com>
2022-09-15 17:02:50 +0100

    working batch implementation

--- tap_snowflake/client.py
+++ tap_snowflake/client.py
@@ -3,11 +3,15 @@
 This includes SnowflakeStream and SnowflakeConnector.
 """
 
-from typing import Any, Dict, Iterable, Optional
+from __future__ import annotations
 
-import sqlalchemy
+from typing import Any, Dict, Iterable, Optional
+from uuid import uuid4
+
 from singer_sdk import SQLConnector, SQLStream
+from singer_sdk.helpers._batch import BaseBatchFileEncoding, BatchConfig
 from snowflake.sqlalchemy import URL
+from sqlalchemy.sql import text
 
 
 class SnowflakeConnector(SQLConnector):
@@ -33,20 +37,41 @@ class SnowflakeStream(SQLStream):
 
     connector_class = SnowflakeConnector
 
-    def get_records(self, partition: Optional[dict]) -> Iterable[Dict[str, Any]]:
-        """Return a generator of row-type dictionary objects.
+    def get_batches(
+        self, batch_config: BatchConfig, context: dict | None = None
+    ) -> Iterable[tuple[BaseBatchFileEncoding, list[str]]]:
+        return self.get_batches_from_internal_stage(batch_config, context)
 
-        Developers may optionally add custom logic before calling the default
-        implementation inherited from the base class.
+    def get_batches_from_internal_stage(
+        self, batch_config: BatchConfig, context: dict | None = None
+    ) -> Iterable[tuple[BaseBatchFileEncoding, list[str]]]:
+        """Unload Snowflake table to User Internal Stage, and download files to local storage.
 
-        Args:
-            partition: If provided, will read specifically from this data slice.
+        This method uses the Internal stage type, into the Snowflake-managed stage created
+        for each user.
 
-        Yields:
-            One dict per record.
+        More details on how this works can be found in the Snowflake docs:
+        https://docs.snowflake.com/en/user-guide/data-unload-snowflake.html#unloading-data-to-your-user-stage
         """
-        # Optionally, add custom logic instead of calling the super().
-        # This is helpful if the source database provides batch-optimized record
-        # retrieval.
-        # If no overrides or optimizations are needed, you may delete this method.
-        yield from super().get_records(partition)
+        root = batch_config.storage.root
+        sync_id = f"{self.tap_name}--{self.name}-{uuid4()}"
+        prefix = batch_config.storage.prefix or ""
+        table_name = self.fully_qualified_name
+        # unload table into user internal stage
+        copy_statement = text(
+            f"copy into '@~/tap-snowflake/{sync_id}/{prefix}' from (select object_construct(*) from {table_name}) file_format = (type='JSON', compression='GZIP') overwrite = TRUE"
+        )
+        self.connector.connection.execute(copy_statement)
+        # list available files
+        results = self.connector.connection.execute(
+            text(f"list '@~/tap-snowflake/{sync_id}/'")
+        )
+        # download available files
+        files = []
+        for result in results:
+            file_name = result[0]
+            self.connector.connection.execute(text(f"get '@~/{file_name}' '{root}/'"))
+            files.append(f"{root}/{file_name}")
+        # remove staged files
+        self.connector.connection.execute(text(f"remove '@~/tap-snowflake/{sync_id}/'"))
+        yield (batch_config.encoding, files)
