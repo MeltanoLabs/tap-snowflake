2a35cccd8a6e38e0098980d57b05a2ecfc981681
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-08-08 17:55:30 -0600

    fix: Work around unsupported `json_serializer` and `json_deserializer`

--- tap_snowflake/client.py
+++ tap_snowflake/client.py
@@ -68,17 +68,11 @@ class SnowflakeConnector(SQLConnector):
 
         return URL(**params)
 
-    def create_sqlalchemy_engine(self) -> sqlalchemy.engine.Engine:
-        """Return a new SQLAlchemy engine using the provided config.
-
-        Developers can generally override just one of the following:
-        `sqlalchemy_engine`, sqlalchemy_url`.
-
-        Returns:
-            A newly created SQLAlchemy engine object.
-        """
+    def create_engine(self) -> sqlalchemy.engine.Engine:
         return sqlalchemy.create_engine(
-            self.sqlalchemy_url, echo=False, pool_timeout=10
+            self.sqlalchemy_url,
+            echo=False,
+            pool_timeout=10,
         )
 
     # overridden to filter out the information_schema from catalog discovery
