63059c2ade5418f3a581ac69f22b84dab5367b96
Nidhi Kakulawaram <nidhi@akk.io>
2023-11-22 07:58:25 -0800

    adding logging again

--- tap_snowflake/client.py
+++ tap_snowflake/client.py
@@ -13,6 +13,7 @@ from typing import Any, Iterable, List, Tuple
 from uuid import uuid4
 import datetime
 
+from loguru import logger
 import sqlalchemy
 from singer_sdk import SQLConnector, SQLStream, metrics
 from singer_sdk.helpers._batch import BaseBatchFileEncoding, BatchConfig
@@ -111,24 +112,25 @@ class SnowflakeConnector(SQLConnector):
         result: list[dict] = []
         tables = [t.lower() for t in self.config.get("tables", [])]
         result.append(self.config)
-        # engine = self.create_sqlalchemy_engine()
-        # inspected = sqlalchemy.inspect(engine)
-        # schema_names = [
-        #     schema_name
-        #     for schema_name in self.get_schema_names(engine, inspected)
-        #     if schema_name.lower() != "information_schema"
-        # ]
-        # LOGGER.debug("This is the schema_names: %s", schema_names)
-        # for schema_name in schema_names:
-        #     # Iterate through each table and view
-        #     for table_name, is_view in self.get_object_names(
-        #         engine, inspected, schema_name
-        #     ):
-        #         if (not tables) or (f"{schema_name}.{table_name}" in tables):
-        #             catalog_entry = self.discover_catalog_entry(
-        #                 engine, inspected, schema_name, table_name, is_view
-        #             )
-        #             result.append(catalog_entry.to_dict())
+        engine = self.create_sqlalchemy_engine()
+        inspected = sqlalchemy.inspect(engine)
+        schema_names = [
+            schema_name
+            for schema_name in self.get_schema_names(engine, inspected)
+            if schema_name.lower() != "information_schema"
+        ]
+        logger.info("This is the schema_names: %s", schema_names)
+        logger.info("This is the tables: %s", tables)
+        for schema_name in schema_names:
+            # Iterate through each table and view
+            for table_name, is_view in self.get_object_names(
+                engine, inspected, schema_name
+            ):
+                if (not tables) or (f"{schema_name}.{table_name}" in tables):
+                    catalog_entry = self.discover_catalog_entry(
+                        engine, inspected, schema_name, table_name, is_view
+                    )
+                    result.append(catalog_entry.to_dict())
 
         return result
 
